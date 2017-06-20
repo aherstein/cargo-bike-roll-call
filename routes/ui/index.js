755c48d55c2dfffcce6fb5ebba905131a8c64417
Adam Herstein <adam.herstein@icloud.com>
2017-06-20 16:50:36 -0700

    Removed old pug-rendered route.

--- routes/ui/index.js
+++ routes/ui/index.js
@@ -71,23 +71,4 @@ router.get('/addbike', function (req, res, next) {
     res.render('addbike', scope);
 });
 
-/* GET bike listing. */
-router.get('/bikes', function (req, res, next) {
-    let options = {
-        method: 'GET',
-        url: 'http://localhost:3000/api/bikes'
-    };
-
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-
-        console.log(body);
-
-        res.render('index', {
-            title: 'Latest Cargo Bikes',
-            "bikes": JSON.parse(body).sort(byDateDesc)
-        });
-    });
-});
-
 module.exports = router;
