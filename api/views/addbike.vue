31652b94b9059d8d5917b6cd79c2dffb9c948045
Adam Herstein <adam.herstein@icloud.com>
2017-06-21 12:01:02 -0700

    Moved backend code to api dir.

---
+++ api/views/addbike.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="add-bike">
+        <bike></bike>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+            return {}
+        }
+    }
+</script>
+
+<style lang="css">
+</style>
